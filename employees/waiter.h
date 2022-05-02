77d68023fe85bb2f20564e65ffc443e8bb3bf3a1
dwyszyns <01168920@pw.edu.pl>
2022-05-02 21:36:49 +0200

    Upload New File

---
+++ employees/waiter.h
@@ -0,0 +1,23 @@
+#ifndef CPP_PROJECT_RECEPCIONIST_H
+#define CPP_PROJECT_RECEPCIONIST_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include "employee.h"
+
+class waiter : public employee
+{
+    public:
+    waiter(std::string, std::string, std::string, std::string, double, std::vector<std::pair<Date, int>>, std::vector<Date>);
+    
+    virtual double workingHours();
+    virtual double salary();
+
+    // double receipt();
+
+    // virtual void makeRoster()=0;
+    // virtual void takeVacation(date)=0;
+    // virtual void removeVacation(date)=0;
+
+};
+#endif
