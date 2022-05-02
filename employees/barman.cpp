2140fc5b08509b58ad62e94bfd5c68ad13764db7
dwyszyns <01168920@pw.edu.pl>
2022-05-02 21:29:50 +0200

    Upload New File

---
+++ employees/barman.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "recepcionist.h"
+#include "employee.h"
+#include "Date.h"
+#include "barman.h"
+
+barman::barman(std::string firstName1, std::string lastName1, std::string emailAdress1, std::string PESEL1, double hourlyRate1, std::vector<std::pair<Date, int>> roster1, std::vector<Date> freeDays1)
+{
+    firstName = firstName1;
+    lastName = lastName1;
+    emailAdress = emailAdress1;
+    PESEL = PESEL1;
+    hourlyRate = hourlyRate1;
+    roster = roster1;
+    freeDays = freeDays1;
+}
+
+double barman::workingHours()
+{
+    return 8*roster.size();
+}
+
+double barman::salary()
+{
+    return hourlyRate*workingHours();
+}
