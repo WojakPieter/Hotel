4afeff9e9b9ad23f439707786a57accfde812b4f
dwyszyns <01168920@pw.edu.pl>
2022-05-02 21:30:14 +0200

    Upload New File

---
+++ employees/bodyguard.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "recepcionist.h"
+#include "employee.h"
+#include "Date.h"
+#include "bodyguard.h"
+
+bodyguard::bodyguard(std::string firstName1, std::string lastName1, std::string emailAdress1, std::string PESEL1, double hourlyRate1, std::vector<std::pair<Date, int>> roster1, std::vector<Date> freeDays1)
+{
+    firstName = firstName1;
+    lastName = lastName1;
+    emailAdress = emailAdress1;
+    PESEL = PESEL1;
+    hourlyRate = hourlyRate1;
+    roster = roster1;
+    freeDays = freeDays1;
+}
+
+double bodyguard::workingHours()
+{
+    return 8*roster.size();
+}
+
+double bodyguard::salary()
+{
+    return hourlyRate*workingHours();
+}
