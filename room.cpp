3a20c919561d408c602b6f5f8e048bbb5238aac1
sspaczyn <01168915@pw.edu.pl>
2022-05-04 00:52:45 +0200

    Fixed problems with pure virtual methods

--- room.cpp
+++ room.cpp
@@ -1,4 +1,3 @@
-#include "date.cpp"
 #include "room.hpp"
 
 double Room::get_area()
@@ -23,6 +22,11 @@ Room::Room(int number, bool high_standard, bool family)
     this->family = family;
 }
 
+OnePersonRoom::OnePersonRoom(int number, bool high_standard, bool family) : Room(number, high_standard, false)
+{
+    this->set_parameters();
+}
+
 bool Room::is_reserved(Date date)
 {
     for(std::pair<Date, Date> interval : reserved_days)
@@ -37,16 +41,137 @@ bool Room::is_high_standard()
     return high_standard;
 }
 
-OnePersonRoom::OnePersonRoom(int number, bool high_standard)
+void OnePersonRoom::set_parameters()
 {
-    this->number = number;
-    this->high_standard = high_standard;
+        if(high_standard)
+        {
+            area = 25;
+            price = 100;
+        }
+        else
+        {
+            area = 15;
+            price = 80;
+        }
 }
 
-Studio::Studio(int number)
+int OnePersonRoom::get_number_of_beds()
 {
-    this->number = number;
+    return 1;
 }
 
+int OnePersonRoom::get_number_of_required_workers()
+{
+    if(high_standard) return 2;
+    else return 1;
+}
 
+void TwoPersonRoom::set_parameters()
+{
+        if(high_standard)
+        {
+            area = 35;
+            price = 110;
+        }
+        else
+        {
+            area = 25;
+            price = 90;
+        }
+}
 
+int TwoPersonRoom::get_number_of_beds()
+{
+    if(family) return 1;
+    else return 2;
+}
+
+int TwoPersonRoom::get_number_of_required_workers()
+{
+    if(high_standard) return 3;
+    else return 1;
+}
+
+void ThreePersonRoom::set_parameters()
+{
+        if(high_standard)
+        {
+            area = 55;
+            price = 130;
+        }
+        else
+        {
+            area = 45;
+            price = 100;
+        }
+}
+
+int ThreePersonRoom::get_number_of_beds()
+{
+    if(family) return 2;
+    else return 3;
+}
+
+int ThreePersonRoom::get_number_of_required_workers()
+{
+    if(high_standard) return 4;
+    else return 1;
+}
+
+void FourPersonRoom::set_parameters()
+{
+        if(high_standard)
+        {
+            area = 65;
+            price = 160;
+        }
+        else
+        {
+            area = 45;
+            price = 120;
+        }
+}
+
+int FourPersonRoom::get_number_of_beds()
+{
+    if(family) return 3;
+    else return 4;
+}
+
+int FourPersonRoom::get_number_of_required_workers()
+{
+    if(high_standard) return 4;
+    else return 2;
+}
+
+void Studio::set_parameters()
+{
+        area = 45;
+        price = 120;
+}
+
+int Studio::get_number_of_beds()
+{
+    return 4;
+}
+
+int Studio::get_number_of_required_workers()
+{
+    return 5;
+}
+
+void Apartment::set_parameters()
+{
+    area = 80;
+    price = 200;
+}
+
+int Apartment::get_number_of_beds()
+{
+    return 1;
+}
+
+int Apartment::get_number_of_required_workers()
+{
+    return 7;
+}
