56c4c708112beb431c943bde86140dbd3f0f47cc
dwyszyns <01168920@pw.edu.pl>
2022-05-04 22:00:47 +0200

    correcting the error of the initialization list

--- employees/employee.h
+++ employees/employee.h
@@ -18,20 +18,27 @@ class employee
 
 
     public:
-    std::string getFirstName();
-    std::string getLastName();
-    std::string getEmailAdress();
-    std::string getPESEL();
-    double getHourlyRate();
+    employee(std::string, std::string, std::string, std::string, double);
+    // ~employee();
+
+    std::string getFirstName() const;
+    std::string getLastName() const;
+    std::string getEmailAdress() const;
+    std::string getPESEL() const;
+    double getHourlyRate() const;
     void setName(std::string, std::string);
     void setEmailAdress(std::string);
     void setPESEL(std::string);
-    void setHourlyRate(double);
-    virtual double workingHours()=0;
-    virtual double salary()=0;
+    virtual void setHourlyRate(double) = 0;
+    virtual double workingHours() = 0;
+    virtual double salary() = 0;
     // virtual void makeRoster()=0;
     // virtual void takeVacation(date)=0;
     // virtual void removeVacation(date)=0;
 
+    bool operator==(const employee& second_employee)
+    {
+        return(PESEL == second_employee.PESEL);
+    }
 };
 #endif
