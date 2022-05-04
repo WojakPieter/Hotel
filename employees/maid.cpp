30b22c988effe5818d4a6f49a10a8f20c97dc0c8
dwyszyns <01168920@pw.edu.pl>
2022-05-04 22:02:20 +0200

    improvement of the constructor and methods

--- employees/maid.cpp
+++ employees/maid.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
-#include "recepcionist.h"
-#include "employee.h"
-#include "Date.h"
+#include <string>
+#include <vector>
 #include "maid.h"
 
-maid::maid(std::string firstName1, std::string lastName1, std::string emailAdress1, std::string PESEL1, double hourlyRate1, std::vector<std::pair<Date, int>> roster1, std::vector<Date> freeDays1)
-{
-    firstName = firstName1;
-    lastName = lastName1;
-    emailAdress = emailAdress1;
-    PESEL = PESEL1;
-    hourlyRate = hourlyRate1;
-    roster = roster1;
-    freeDays = freeDays1;
-}
+maid::maid(std::string firstName, std::string lastName, std::string emailAdress, std::string PESEL, double hourlyRate):
+employee(firstName, lastName, emailAdress, PESEL, hourlyRate)
+{}
 
 double maid::workingHours()
 {
-    return 8*roster.size();
+    int days = roster.size();
+    return 9*days;
 }
 
 double maid::salary()
 {
     return hourlyRate*workingHours();
+}
+
+void maid::setHourlyRate(double newRate) {
+    if (newRate > 0)
+        hourlyRate = newRate;
+    else
+        throw std::range_error("Stawka godzinowa musi byc wieksza od zera");
 }
