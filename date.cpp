a788b58bc6dd42aa85974fff3cffd3c807eaf68d
sspaczyn <01168915@pw.edu.pl>
2022-05-06 21:16:27 +0200

    Added new void controling inputs

--- date.cpp
+++ date.cpp
@@ -6,9 +6,9 @@ public:
     int day, month, year;
     Date(int day=0, int month=0, int year=0)
     {
-        this->set_day(day);
-        this->set_month(month);
         this->set_year(year);
+        this->set_month(month);
+        this->set_day(day);
     }
     int get_day()
     {
@@ -55,20 +55,30 @@ public:
         }
     }
 
+    void control_inputs()
+    {
+        if(day < 1 || day > get_month_length(month))
+            throw std::out_of_range("Day out of range");
+        if(month < 1 || month > 12)
+            throw std::out_of_range("Month out of range");
+        if(year < 1)
+            throw std::out_of_range("Year cannot be negative");
+    }
+
     void set_day(int new_day)
     {
-        if(new_day < 1 || new_day > get_month_length(month)) throw std::out_of_range("Day out of range");
         day = new_day;
+        this->control_inputs();
     }
     void set_month(int new_month)
     {
-        if(new_month < 1 || new_month > 12) throw std::out_of_range("Month out of range");
         month = new_month;
+        this->control_inputs();
     }
     void set_year(int new_year)
     {
-        if(year < 1) throw std::out_of_range("Year cannot be negative");
         year = new_year;
+        this->control_inputs();
     }
 
     bool operator<(const Date& second_date)
