40e012276682c4e5d0f251aa3e7e85ff453fb1b6
sspaczyn <01168915@pw.edu.pl>
2022-05-12 21:43:39 +0200

    Added method 'set_parameters'

--- room.hpp
+++ room.hpp
@@ -1,38 +1,50 @@
 #include "date.cpp"
 #include <iostream>
 #include <vector>
+#include <map>
 
 class Room
 {
 protected:
+    char type;
     double area;
     double price;
     bool high_standard;
     bool family;
     int number;
-    std::vector<std::pair<Date, Date>> reserved_days;
+    double base_price;
+    double base_area;
+    std::vector<Date> reserved_days;
 
 public:
     Room(int number, bool high_standard, bool family);
+    void add_reserved_day(Date);
+    void remove_reserved_day(Date);
+    char get_type();
     double get_area();
     int get_price();
+    bool is_family();
     bool is_high_standard();
     int get_number();
     virtual int get_number_of_required_workers()=0;
     virtual int get_number_of_beds()=0;
-    virtual void set_parameters()=0;
+    virtual void set_parameters(double price_multiplier_high, double area_multiplier_high,
+                        double price_multiplier_low, double area_multiplier_low);
     bool is_reserved(Date);
+    //
 
     bool operator==(const Room& second_room)
     {
         return(number == second_room.number);
     }
+
+    friend std::ostream& operator<<(std::ostream& os, Room&);
 };
 
 class OnePersonRoom : public Room
 {
 public:
-    OnePersonRoom(int number, bool high_standard, bool family);
+    OnePersonRoom(int number, bool high_standard);
     virtual int get_number_of_required_workers();
     virtual int get_number_of_beds();
     virtual void set_parameters();
